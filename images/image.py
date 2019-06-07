757e5a6c64f78ee511e6b91ce08621df7634ba1e
Rodolfo González <rodolfo.gonzalez@globant.com>
2019-06-07 17:17:54 -0500

    c image size

--- images/image.py
+++ images/image.py
@@ -1,14 +1,7 @@
 from PIL import Image
 import sys
 
-def merge(image_a, image_b, output):
-    pass
-
-def cut_Y(image):
-    pass
-
-def main(output, images):
-    k = len(images)
+def get_image_size(images):
     c = {
         "width": 0,
         "height": 0,
@@ -18,11 +11,19 @@ def main(output, images):
         width, height = Image.open(image).size
         c['width']+= width
         c['height']+= height
+        print("{} {}".format(width, height))
 
-        print("width: {} height: {}".format(width, height))
-        print("width: {} height: {}".format(c['width'], c['height']))
+    c['width']  = int(c['width']/len(images))
+    c['height'] = int(c['height']/len(images))
+    return c
+
+def merge(image_a, image_b, output):
     pass
 
+
+def main(output, images):
+    c = get_image_size(images)
+    print("{} {}".format(c['width'] ,c['height']))
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2:])
