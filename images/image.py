c588557a47e0e494361ee6920a3e1c466dbef323
Rodolfo González <rodolfo.gonzalez@globant.com>
2019-06-07 17:05:18 -0500

    image sizes

--- images/image.py
+++ images/image.py
@@ -1,12 +1,28 @@
-def get_size(image):
-    pass
-def merge(parameter_list):
+from PIL import Image
+import sys
+
+def merge(image_a, image_b, output):
     pass
 
-def main(image_a, image_b, output):
+def cut_Y(image):
+    pass
 
+def main(output, images):
+    k = len(images)
+    c = {
+        "width": 0,
+        "height": 0,
+    }
+
+    for key,image in enumerate(images):
+        width, height = Image.open(image).size
+        c['width']+= width
+        c['height']+= height
+
+        print("width: {} height: {}".format(width, height))
+        print("width: {} height: {}".format(c['width'], c['height']))
     pass
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    main(sys.argv[1], sys.argv[2:])
