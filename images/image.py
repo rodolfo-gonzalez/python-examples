5c727c575e37835546afb720132af53989e0461a
Rodolfo González <rodolfo.gonzalez@globant.com>
2019-06-07 16:19:54 -0500

    initial commit

---
+++ images/image.py
@@ -0,0 +1,12 @@
+def get_size(image):
+    pass
+def merge(parameter_list):
+    pass
+
+def main(image_a, image_b, output):
+
+    pass
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
