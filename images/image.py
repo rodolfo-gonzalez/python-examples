5e1cf9cd66247e3305204d897f4885cd833d85dd
Rodolfo González <rodolfo.gonzalez@globant.com>
2019-06-07 17:39:21 -0500

    merde

--- images/image.py
+++ images/image.py
@@ -11,19 +11,29 @@ def get_image_size(images):
         width, height = Image.open(image).size
         c['width']+= width
         c['height']+= height
-        print("{} {}".format(width, height))
 
     c['width']  = int(c['width']/len(images))
     c['height'] = int(c['height']/len(images))
     return c
 
-def merge(image_a, image_b, output):
-    pass
+def merge(images, c):
+    c_image = None
+    for key,image_path in enumerate(images):
+        image = Image.open(image_path)
+        image = image.resize((c['width'],c['height'])).convert("RGBA")
+
+        if key == 0:
+            c_image = image
+
+        c_image = Image.blend(c_image, image, alpha=.4)
+    return c_image.convert("RGB")
 
 
 def main(output, images):
     c = get_image_size(images)
-    print("{} {}".format(c['width'] ,c['height']))
+
+    merge(images, c).save(output + "/c_image.jpg")
+    merge(images, c).show()
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2:])
